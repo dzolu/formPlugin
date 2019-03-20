33c6a33b7f5f86ab9b06adfe9e1f6b40c5a940af
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2019-03-20 12:28:44 +0100

    style: char limit

--- limitCharPlugin.js
+++ limitCharPlugin.js
@@ -4,11 +4,19 @@
            var limitElem= $("#form__char-left__number");
            var limit=$(this).attr("data-max-length");
             limitElem.text(limit);
-            $(this).keypress(function (event) {
-                if($(this).val().length>=5){
+            $(this).keypress(function () {
+                if($(this).val().length>=limit){
                     return false
                 }
-                limitElem.text(limit-$(this).val().length)
+            });
+
+            $(this).on("change keyup paste", function () {
+               limitElem.text(limit-$(this).val().length);
+               // if user paste to much chars then cut
+                if($(this).val().length>limit){
+                    $(this).val($(this).val().substr(0, limit));
+                    limitElem.text(limit-$(this).val().length);
+                }
             })
 
         })
